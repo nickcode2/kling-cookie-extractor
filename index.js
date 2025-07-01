37bf1ea9b8b567bb1b6941a2601f245e5bfd2f49
Rudson Nick <44381007-rudsonnick@users.noreply.replit.com>
2025-07-01 04:10:04 +0000

    fix: switch to puppeteer-core and add apt.txt

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const PORT = 3000;
 
 app.get("/", async (req, res) => {
   const browser = await puppeteer.launch({
-    executablePath: "/usr/bin/chromium-browser",
+    executablePath: "/usr/bin/chromium", // ⬅️ Not chromium-browser
     args: ["--no-sandbox", "--disable-setuid-sandbox"],
     headless: true,
   });
